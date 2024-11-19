c2a5013fa4c7bd564ced266761fd924c7249c99a
mzk <mizuki.hitomipeeq@gmail.com>
2024-11-19 09:44:33 +0900

    Updates

--- build/events/natsSetup.js
+++ build/events/natsSetup.js
@@ -11,19 +11,12 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.natsSetup = void 0;
 const nats_1 = require("nats");
-const exit = (nc) => __awaiter(void 0, void 0, void 0, function* () {
-    yield nc.close();
-    process.exit();
-});
 const natsSetup = (...args_1) => __awaiter(void 0, [...args_1], void 0, function* (url = 'http://localhost:4222') {
     const nc = yield (0, nats_1.connect)({
         servers: url
     });
     const jsm = yield nc.jetstreamManager();
     const js = nc.jetstream();
-    process.on('SIGINT', exit);
-    process.on('SIGTERM', exit);
-    process.on('exit', exit);
     return { nc, jsm, js };
 });
 exports.natsSetup = natsSetup;
